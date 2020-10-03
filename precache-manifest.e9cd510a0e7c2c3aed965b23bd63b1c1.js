b10a434ecbf0d51165cd518d1bfcb57a272083e0
Sergio Picca <sergiopicca39@gmail.com>
2020-10-03 14:36:07 +0200

    Updates

---
+++ precache-manifest.e9cd510a0e7c2c3aed965b23bd63b1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a82f97c65da532b6d7a4d6aa18afe4d",
+    "url": "/lowPolyRobotsAttack/index.html"
+  },
+  {
+    "revision": "b5cbb2a9e45405dfed09",
+    "url": "/lowPolyRobotsAttack/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51c6fd0a796e3689aeb3",
+    "url": "/lowPolyRobotsAttack/static/js/2.982db042.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lowPolyRobotsAttack/static/js/2.982db042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cbb2a9e45405dfed09",
+    "url": "/lowPolyRobotsAttack/static/js/main.eeba4ec1.chunk.js"
+  },
+  {
+    "revision": "8317a05591a569bbbbe6",
+    "url": "/lowPolyRobotsAttack/static/js/runtime-main.f23141bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lowPolyRobotsAttack/static/media/logo.5d5d9eef.svg"
+  }
+]);

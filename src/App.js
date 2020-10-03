3b939ce8c91a7eb00ae3251607dd3714c5271c5c
Sergio Picca <sergiopicca39@gmail.com>
2020-10-03 14:35:30 +0200

    deploy the react app to github pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>This will be low poly robots attack doc!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

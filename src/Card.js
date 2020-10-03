448a4d7725fe75c5c334c933f1098480d1b33de4
Sergio Picca <sergiopicca39@gmail.com>
2020-10-03 19:49:34 +0200

    Add cards to main page

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 class Card extends React.Component {
- render(props) {
+ constructor(props){
+  super()
+  this.props = props
+ }
+ 
+ render() {
    return (
    <div className='card'>
     <div>
-     <h1 className='level'>{props.level}</h1>
+     <h1 className='level'>{this.props.level}</h1>
     </div>
-    <h3>{props.title}</h3>
-   <p>{props.description}</p>
+    <h3>{this.props.title}</h3>
+   <p>{this.props.desc}</p>
    <button className='access-level'>GO!</button>
    </div>)
  }
--- src/Selection.js
+++ src/Selection.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Card from './Card'
 
 class Selection extends React.Component {
  render() {
+  const firstCard = {level: 'LVL1', title: 'Game and models', desc: 'Take a look on what is the logic of the and how the  models are made!'}
+  const secondCard = {level: 'LVL3', title: 'Technology', desc: 'Read about the technology used and some of the requirements.'}
+  const thirdCard = {level: 'LVL3', title: 'Animations', desc: 'Discover how we implemented the animations of all the characters.'}
+  const playCard = {level: 'PLAY', title: 'No title', desc: 'Enjoy the game  kill all the Robots and be  the champion!'}
+
    return (
    <div className="lvl-selection">
+    <div className="card-container">
+     {new Card(firstCard).render()}
+     {new Card(secondCard).render()}
+     {new Card(thirdCard).render()}
+     {new Card(playCard).render()}
+    </div>
    </div>)
  }
 }

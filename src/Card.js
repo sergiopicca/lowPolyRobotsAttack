dc98c549e96c6809accfab51d7f8ab6ba3d78c38
Sergio Picca <sergiopicca39@gmail.com>
2020-10-04 11:25:22 +0200

    Make the website fully responsive

--- src/Card.js
+++ src/Card.js
@@ -12,7 +12,7 @@ class Card extends React.Component {
     <div>
      <h1 className='level'>{this.props.level}</h1>
     </div>
-    <h3>{this.props.title}</h3>
+    <h4>{this.props.title}</h4>
    <p>{this.props.desc}</p>
    <button className='access-level'>GO!</button>
    </div>)

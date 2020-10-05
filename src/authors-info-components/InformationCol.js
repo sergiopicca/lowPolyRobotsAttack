db1f49eeb8e8f4f052aaaaa0f9864df289e9e78b
Sergio Picca <sergiopicca39@gmail.com>
2020-10-05 17:40:05 +0200

    Change the information in the Sergio section

--- src/authors-info-components/InformationCol.js
+++ src/authors-info-components/InformationCol.js
@@ -5,7 +5,7 @@ import andreapxl  from '../img/andrea_pixeled.png'
 
 class InformationCol extends React.Component {
  render() {
-   const sergio = {name: 'Sergio', task: 'Hi, everyone I worked mainly on the game logic and the model creation forthe main character.', src: sergiopxl, git: 'https://github.com/sergiopicca', lnk:'https://www.linkedin.com/in/sergio-picca-801b0b173/'}
+   const sergio = {name: 'Sergio', task: 'Hi, everyone I worked mainly on the game logic and the model creation for the main character.', src: sergiopxl, git: 'https://github.com/sergiopicca', lnk:'https://www.linkedin.com/in/sergio-picca-801b0b173/'}
    const andrea = {name: 'Andrea', task: 'Hello guys, I worked on the models and on the environment, including lights and shadows.', src: andreapxl, git: 'https://github.com/andreanapoletani', lnk:'https://www.linkedin.com/in/andrea-napoletani-aa0b87166/'}
    return (
     <div className='information-col'>

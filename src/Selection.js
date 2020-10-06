4f6ad2c3d2147bf225f16b00877880153f063566
Sergio Picca <sergiopicca39@gmail.com>
2020-10-06 17:59:18 +0200

    Responsive text

--- src/Selection.js
+++ src/Selection.js
@@ -3,7 +3,7 @@ import Card from './Card'
 
 class Selection extends React.Component {
  render() {
-  const firstCard = {level: 'LVL1', title: 'Game and models', desc: 'Take a look on what is the logic of the and how the  models are made!'}
+  const firstCard = {level: 'LVL1', title: 'Game & models', desc: 'Take a look on the game logic and how the  models are made!'}
   const secondCard = {level: 'LVL2', title: 'Technology', desc: 'Read about the technology used and some of the requirements.'}
   const thirdCard = {level: 'LVL3', title: 'Animations', desc: 'Discover how we implemented the animations of all the characters.'}
   const playCard = {level: 'PLAY', title: 'No title', desc: 'Enjoy the game  kill all the Robots and be  the champion!'}
